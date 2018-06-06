bfc927d31f3838d4209e5a3299f94f30b6de51d9
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-06-06 08:21:28 -0600

    Se agrego la clase Cajero y el metodpo run

---
+++ src/hilosclase/Cajero.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hilosclase;
+
+/**
+ *
+ * @author Oscar Lovato
+ */
+public class Cajero extends Thread{
+    private String nombre;
+    private Cliente cliente,
+    private Transaccion trans;
+
+    public Cajero() {
+    }
+
+    public Cajero(String nombre, Cliente cliente, Transaccion trans) {
+        this.nombre = nombre;
+        this.cliente = cliente;
+        this.trans = tiempoInicio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public long getTiempoInicio() {
+        return trans;
+    }
+
+    public void setTiempoInicio(long tiempoInicio) {
+        this.trans = tiempoInicio;
+    }
+     
+    public void run(){
+        
+    }
+}
